a7f44ad4735ec5f643bc9ce13c18adecff23dbbb
cfrancksupdevinci <cyrille.franck@supdevinci-edu.fr>
2025-05-24 17:25:18 +0200

    clean code

--- Lootopia/frontend/components/ui/dashboard/UserRow.tsx
+++ Lootopia/frontend/components/ui/dashboard/UserRow.tsx
@@ -1,41 +1,3 @@
-// import { ToggleSwitch } from "@/components/ui/dashboard/ToggleSwitch";
-
-// interface UserRowProps {
-//   user: any;
-//   onToggleStatus: (userId: number, currentStatus: number) => void;
-//   onChangeRole: (userId: number, newRole: string) => void;
-//   onBanClick: (userId: number) => void;
-// }
-
-// export const UserRow = ({ user, onToggleStatus, onChangeRole, onBanClick }: UserRowProps) => {
-//   return (
-//     <div className="dashboard-table-row">
-//       <span className="dashboard-cell">
-//         <ToggleSwitch
-//           checked={user.status === 1}
-//           onChange={() => onToggleStatus(user.id, user.status)}
-//         />
-//         <button className="ban-button" onClick={() => onBanClick(user.id)}>
-//           Bannir
-//         </button>
-//       </span>
-//       <span className="dashboard-cell">{user.username}</span>
-//       <span className="dashboard-cell">{user.email}</span>
-//       <span className="dashboard-cell">
-//         <select
-//           value={user.role}
-//           onChange={(e) => onChangeRole(user.id, e.target.value)}
-//           className="role-select"
-//         >
-//           <option value="admin">Admin</option>
-//           <option value="user">User</option>
-//           <option value="moderator">Modérateur</option>
-//           <option value="organizer">Organisateur</option>
-//         </select>
-//       </span>
-//     </div>
-//   );
-// };
 import { useEffect, useState } from "react";
 import { ToggleSwitch } from "@/components/ui/dashboard/ToggleSwitch";
 
@@ -55,7 +17,7 @@ export const UserRow = ({ user, onToggleStatus, onChangeRole, onBanClick }: User
     const interval = setInterval(() => {
       const start = new Date(user.disable_start).getTime();
       const now = Date.now();
-      const banDurationMs = user.disable_time * 60 * 1000; // minutes to ms
+      const banDurationMs = user.disable_time * 60 * 1000;
       const end = start + banDurationMs;
       const diff = end - now;
 
