3dacb4bb4739786412ba6690b530887a9632537f
cfrancksupdevinci <cyrille.franck@supdevinci-edu.fr>
2025-06-12 18:00:39 +0200

    android nav

--- Lootopia/frontend/app/profile.tsx
+++ Lootopia/frontend/app/profile.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 export const ProfileScreen = () => {
   return (
-    <div>
-      <h1>Mon Profil</h1>
-      <p>Ici s'afficheront les infos de l'utilisateur.</p>
-    </div>
+    <View style={styles.container}>
+      <Text style={styles.title}>Mon Profil</Text>
+      <Text style={styles.paragraph}>Ici s'afficheront les infos de l'utilisateur.</Text>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  paragraph: {
+    fontSize: 18,
+  },
+});
 
 export default ProfileScreen;
